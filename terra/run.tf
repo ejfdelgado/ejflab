d31af873e3197500e6ab5f13ef9c1475d24325e9
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2023-05-10 00:12:46 -0500

    working well in cloud run

--- terra/run.tf
+++ terra/run.tf
@@ -1,32 +1,35 @@
-resource "google_cloud_run_service" "run_service" {
+resource "google_cloud_run_v2_service" "mainapp" {
   name     = "mainapp"
   location = var.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
-    spec {
-      containers {
-        image = "gcr.io/ejfexperiments/mainapp:v1.0"
+    max_instance_request_concurrency = 80
+    containers {
+      image = "gcr.io/ejfexperiments/mainapp:v1.0"
+      resources {
+        limits = {
+          # 512Mi
+          memory = "1Gi"
+          # '1', '2', '4', and '8' 1000m 250m 500m
+          cpu = "1000m"
+        }
       }
     }
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 10
+    }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-
-  # Waits for the Cloud Run API to be enabled
-  depends_on = [google_project_service.run_api]
 }
 
 resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.run_service.name
-  location = google_cloud_run_service.run_service.location
+  service  = google_cloud_run_v2_service.mainapp.name
+  location = google_cloud_run_v2_service.mainapp.location
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
-# Display the service URL
 output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
+  value = google_cloud_run_v2_service.mainapp.traffic_statuses[0].uri
 }
