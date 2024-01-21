16b76d51e28450116de74778e6d00bc4d14a1872
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2024-01-21 10:19:45 -0500

    finished gallery

--- src/app/libs/threejs/threejs-gallery/BasicScene.ts
+++ src/app/libs/threejs/threejs-gallery/BasicScene.ts
@@ -80,6 +80,48 @@ export class BasicScene extends THREE.Scene {
     this.renderer.setSize(this.bounds.width, this.bounds.height);
   }
 
+  fitCameraToSelection(
+    camera: any,
+    controls: any,
+    selection: any,
+    fitOffset = 1.2
+  ) {
+    const size = new THREE.Vector3();
+    const center = new THREE.Vector3();
+    const box = new THREE.Box3();
+
+    box.makeEmpty();
+    for (const object of selection) {
+      box.expandByObject(object);
+    }
+
+    box.getSize(size);
+    box.getCenter(center);
+
+    const maxSize = Math.max(size.x, size.y, size.z);
+    const fitHeightDistance =
+      maxSize / (2 * Math.atan((Math.PI * camera.fov) / 360));
+    const fitWidthDistance = fitHeightDistance / camera.aspect;
+    const distance = fitOffset * Math.max(fitHeightDistance, fitWidthDistance);
+
+    const direction = controls.target
+      .clone()
+      .sub(camera.position)
+      .normalize()
+      .multiplyScalar(distance);
+
+    controls.maxDistance = distance * 10;
+    controls.target.copy(center);
+
+    camera.near = distance / 100;
+    camera.far = distance * 100;
+    camera.updateProjectionMatrix();
+
+    camera.position.copy(controls.target).sub(direction);
+
+    controls.update();
+  }
+
   async addFBXModel(item: ItemModelRef): Promise<void> {
     // Remove previous model
     if (this.lastObject != null) {
@@ -91,6 +133,7 @@ export class BasicScene extends THREE.Scene {
         (object) => {
           this.lastObject = object;
           this.add(object);
+          this.fitCameraToSelection(this.camera, this.orbitals, [object]);
           resolve();
         },
         (xhr) => {
